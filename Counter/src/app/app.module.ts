7723d840ad2108c0de93674c97da141c951e64c2
stego135 <fbecker@ceiamerica.com>
2022-07-13 11:21:44 -0400

    finish pokemon detail page

--- Counter/src/app/app.module.ts
+++ Counter/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { PokemonService } from './pokemon-service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [PokemonService],
   bootstrap: [AppComponent]
--- Counter/src/app/pokemon-detail/pokemon-detail.component.ts
+++ Counter/src/app/pokemon-detail/pokemon-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import {HttpClient} from '@angular/common/http';
+import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon-service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-detail',
@@ -6,10 +12,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon-detail.component.css']
 })
 export class PokemonDetailComponent implements OnInit {
+  name!: string
+  pokemon: Pokemon = new Pokemon;
+  pokemonList!: Observable<Pokemon[]>
+  data: any
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, 
+    private pokemonService: PokemonService,
+    private http:HttpClient,
+    private location: Location) { }
 
   ngOnInit(): void {
+    this.getName();
+    this.getPokemon();
+    this.getImageUrl();
+  }
+  getName(): void {
+    this.name = String(this.route.snapshot.paramMap.get('name'));
+  }
+  getPokemon() {
+    this.pokemonList = this.pokemonService.getPokemon();
+    this.pokemon = this.pokemonService.getOnePokemon(this.name);
+  }
+  getImageUrl() {
+    var lowerName = new String(this.pokemon.name);
+    lowerName  = lowerName[0].toLowerCase() + lowerName.slice(1);
+    this.http.get("https://pokeapi.co/api/v2/pokemon/" + lowerName).subscribe(data=> {
+      this.data = data;
+    });
+  }
+  add() {
+    this.pokemon.count+=1;
+  }
+  goBack(): void {
+    this.location.back();
   }
 
 }
