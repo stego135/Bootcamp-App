da9feb12292b2ca1308edcf4386e7cff2cfa8f1e
stego135 <fbecker@ceiamerica.com>
2022-07-13 16:22:52 -0400

    added images, working on css

--- Counter/src/app/pokemon-thumbnail/pokemon-thumbnail.component.ts
+++ Counter/src/app/pokemon-thumbnail/pokemon-thumbnail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-pokemon-thumbnail',
@@ -8,10 +9,21 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PokemonThumbnailComponent implements OnInit {
 
   @Input() pokemon:any
+  data:any
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+  }
 
   ngOnInit(): void {
+    this.getImageUrl();
+  }
+
+  getImageUrl() {
+    var lowerName = new String(this.pokemon.name);
+    lowerName  = lowerName[0].toLowerCase() + lowerName.slice(1);
+    this.http.get("https://pokeapi.co/api/v2/pokemon/" + lowerName).subscribe(data=> {
+      this.data = data;
+    });
   }
 
 }
