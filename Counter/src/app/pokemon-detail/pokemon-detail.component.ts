a56f7fd0c089d4e6cd84454ef2de794d99cfdd95
stego135 <fbecker@ceiamerica.com>
2022-07-14 15:34:46 -0400

    fixed typing error

--- Counter/src/app/pokemon-detail/pokemon-detail.component.ts
+++ Counter/src/app/pokemon-detail/pokemon-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon-service';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-detail',
@@ -9,14 +12,22 @@ import { Location } from '@angular/common';
 })
 export class PokemonDetailComponent implements OnInit {
   name!: String
+  pokemon: Pokemon = new Pokemon;
+  pokemonList!: Pokemon[]
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private pokemonService: PokemonService) { }
 
   ngOnInit(): void {
     this.getName();
+    this.getPokemon();
   }
   getName(): void {
     this.name = String(this.route.snapshot.paramMap.get('name'));
+  }
+  getPokemon() {
+    this.pokemonList = this.pokemonService.getPokemon();
+    let filteredList = this.pokemonList.find(pokemon => pokemon.name == this.name);
+    if (filteredList) this.pokemon = filteredList;
   }
 
 }
