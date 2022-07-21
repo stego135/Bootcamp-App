4ac029bb88f70d0ca3e819107236a997cb70d4df
stego135 <fbecker@ceiamerica.com>
2022-07-21 11:57:52 -0400

    buttons change color when active

--- Counter/src/app/hall-of-fame/hall-of-fame.component.ts
+++ Counter/src/app/hall-of-fame/hall-of-fame.component.ts
@@ -9,6 +9,9 @@ import { Pokemon } from '../shared/pokemon';
 })
 export class HallOfFameComponent implements OnInit {
   shiny!: Pokemon[]
+  isTime: Boolean = true;
+  isAsc: Boolean = false;
+  isDesc: Boolean = false;
 
   constructor(private hallOfFameService: HallOfFameService) { }
 
@@ -18,12 +21,21 @@ export class HallOfFameComponent implements OnInit {
   
   getShiny() {
     this.shiny = this.hallOfFameService.getShiny();
+    this.isTime = true;
+    this.isAsc = false;
+    this.isDesc = false;
   }
   sortAsc() {
     this.shiny = this.hallOfFameService.sortAsc();
+    this.isTime = false;
+    this.isAsc = true;
+    this.isDesc = false;
   }
   sortDesc() {
     this.shiny = this.hallOfFameService.sortDesc();
+    this.isTime = false;
+    this.isAsc = false;
+    this.isDesc = true;
   }
 
 }
