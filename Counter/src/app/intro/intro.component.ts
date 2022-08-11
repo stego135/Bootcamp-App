3a191e8ff4d91d92da1410acfb7162471d24b00e
stego135 <fbecker@ceiamerica.com>
2022-08-11 11:02:43 -0400

    navbar now redirects to intro page

--- Counter/src/app/intro/intro.component.ts
+++ Counter/src/app/intro/intro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserService } from '../shared/user-service';
 
 @Component({
   selector: 'app-intro',
@@ -6,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./intro.component.css']
 })
 export class IntroComponent implements OnInit {
+  isLoggedIn: Observable<boolean>;
 
-  constructor() { }
+  constructor(private userService: UserService) { 
+    this.isLoggedIn = this.userService.getLogIn();
+  }
 
   ngOnInit(): void {
   }
