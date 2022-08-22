a2d14bf10cbe98b912dae50f51db5540eef747a5
stego135 <fbecker@ceiamerica.com>
2022-08-22 15:38:08 -0400

    pokemon thumbnail tests

--- Counter/src/app/pokemon-thumbnail/pokemon-thumbnail.component.spec.ts
+++ Counter/src/app/pokemon-thumbnail/pokemon-thumbnail.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Observable, of } from 'rxjs';
+import { Pokemon } from '../shared/pokemon';
 import { PokemonService } from '../shared/pokemon-service';
 
 import { PokemonThumbnailComponent } from './pokemon-thumbnail.component';
@@ -9,7 +10,7 @@ describe('PokemonThumbnailComponent', () => {
   let fixture: ComponentFixture<PokemonThumbnailComponent>;
   let pokemonServiceStub: Partial<PokemonService> = {
     getImage(): Observable<string> {
-      return of("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/25.png");
+      return of("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/150.png");
     }
   };
 
@@ -22,10 +23,31 @@ describe('PokemonThumbnailComponent', () => {
 
     fixture = TestBed.createComponent(PokemonThumbnailComponent);
     component = fixture.componentInstance;
+    component.pokemon = {id: 6, name: "Mewtwo", count: 50, userId: 1};
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Correct display', () => {
+    it('should display the correct Pokemon name and count', () => {
+      let pokeName = fixture.debugElement.nativeElement.querySelector('.pokemon-name');
+
+      expect(pokeName.textContent).toEqual("Mewtwo");
+    }),
+
+    it('should display the correct image', () => {
+      let pokeCount = fixture.debugElement.nativeElement.querySelector('.pokemon-count');
+
+      expect(pokeCount.textContent).toEqual("Count: 50");
+    }),
+
+    it('should display the correct image', () => {
+      let pokeImg = fixture.debugElement.nativeElement.querySelector('.poke-icon');
+
+      expect(pokeImg.src).toEqual("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/150.png");
+    })
+  })
 });
